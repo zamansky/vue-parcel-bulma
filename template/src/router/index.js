d33ceb2cc345de769396fbe9cff00e4201b280da
Mike Zamansky <zamansky@gmail.com>
2018-01-13 17:35:50 -0500

    removed test component

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '../components/Hello'
-import test from '../components/test'
 
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-      {path: '/', name: 'Hello', component: Hello},
-      {path: '/test', name: 'test', component: test}
+      {path: '/', name: 'Hello', component: Hello}
+      
   ]
 })
